1480cb44013641fd55c966852026ef94adba1217
wyldckat <wyldckat@gmail.com>
2015-05-04 17:27:48 +0100

    Adicionado script para atualizar o número de downloads automaticamente.

---
+++ bin/updateDownStats.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -e
+#
+# crontab -e
+#
+#  * */1 *   *   *     /home/$USER/github/asstermo/asstermo.github.io/bin/updateDownStats.sh
+
+
+cd ${0%/*} || exit 1
+
+./updateDownloads.py > ../_include/downstats.md
+
+if ! git diff ../_include/downstats.md --quiet --exit-code; then
+
+  git add ../_include/downstats.md && \
+  git commit -m "Estatísticas de downloads atualizada." && \
+  git pull && \
+  git push
+
+fi
