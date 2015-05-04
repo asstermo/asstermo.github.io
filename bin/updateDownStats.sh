9d0dd1a0b155675b83d892edd5f7511e9ea63685
wyldckat <wyldckat@gmail.com>
2015-05-04 17:40:58 +0100

    Correcção do script de atualização dos downloads

--- bin/updateDownStats.sh
+++ bin/updateDownStats.sh
@@ -2,14 +2,14 @@
 #
 # crontab -e
 #
-#  * */1 *   *   *     /home/$USER/github/asstermo/asstermo.github.io/bin/updateDownStats.sh
+#  0  *   *   *   *     /home/$USER/github/asstermo/asstermo.github.io/bin/updateDownStats.sh
 
 
 cd ${0%/*} || exit 1
 
 ./updateDownloads.py > ../_includes/downstats.md
 
-if ! git diff -- ../_include/downstats.md --quiet --exit-code; then
+if ! git diff --quiet --exit-code -- ../_includes/downstats.md; then
 
   git add ../_include/downstats.md && \
   git commit -m "Estatísticas de downloads atualizada." && \
